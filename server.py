7f982cb6862f5d2d4e04e3d2ad9bc43f8a875aaf
PosticaDenis <postica.denis.95@gmail.com>
2018-05-22 07:02:57 -0700

    update server script

--- server.py
+++ server.py
@@ -8,24 +8,18 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
+options = {    "w" : car.forward(TF, 100, 100, 100, 100),
+               "s" : car.reverse(TF, 100, 100, 100, 100),
+               "a" : car.fullleft(TF, 100, 100, 100, 100),
+               "d" : car.fullright(TF, 100, 100, 100, 100),
+               "q" : car.forward(TF, 25, 25, 100, 100),
+               "e" : car.forward(TF, 100, 100, 25, 25),
+}
+
 try:
     while True:
-        data, addr = sock.recvfrom(1024)
-        data = data.decode()
-        print('Received: ' + data)
-        if data.lower() == 'w':
-            car.forward(TF, 100, 100, 100, 100)
-        elif data.lower() == 's':
-            car.reverse(TF, 100, 100, 100, 100)
-        elif data.lower() == 'a':
-            car.fullleft(TF, 100, 100, 100, 100)
-        elif data.lower() == 'd':
-            car.fullright(TF, 100, 100, 100, 100)
-        elif data.lower() == 'q':
-            car.forward(TF, 25, 25, 100, 100)
-        elif data.lower() == 'e':
-            car.forward(TF, 100, 100, 25, 25)
-        else:
-            print("Hell no to the no no ...")
+        option, address = sock.recvfrom(256)
+        options[option]
+        
 except KeyboardInterrupt:
     sock.close()
