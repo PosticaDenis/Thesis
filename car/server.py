08f639724bcd639cd35bfb9b6e68180f7d613896
PosticaDenis <postica.denis.95@gmail.com>
2018-05-23 09:51:40 -0700

    speed modif

--- car/server.py
+++ car/server.py
@@ -19,7 +19,7 @@ try:
             print("Received command: ", option)
             
             if option == 'w':
-                car.forward(TF, 75, 75, 75, 75)
+                car.forward(TF, 60, 60, 60, 69)
             elif option == 's':
                 car.reverse(TF, 75, 75, 75, 75)
             elif option == 'a':
