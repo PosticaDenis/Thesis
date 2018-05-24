606284ef9a7b2ae8357aacb6c4e734fc88f35d25
PosticaDenis <postica.denis.95@gmail.com>
2018-05-24 21:44:48 +0300

    Update test_nn.py

--- test_nn.py
+++ test_nn.py
@@ -8,19 +8,19 @@ Created on Thu May 24 02:55:32 2018
 
 import cv2
 import time
-from tools.keyboard_controller import PressKey, ReleaseKey, W, A, S, D, Q, E
+from tools.keyboard_controller import PressKey, ReleaseKey, W, A, D, Q, E
 from alexnet import alexnet
 from tools.keys import check_key
 from tools.screen import get_screen
 
-WIDTH = 292
-HEIGHT = 255
+WIDTH = 90
+HEIGHT = 80
 
 LR = 1e-3
 
 EPOCHS = 10
 
-MODEL_NAME = 'models/autonomous-car-{}-{}-{}-epochs-xK-data.model'.format(LR, 'alexnetv2',EPOCHS)
+MODEL_NAME = 'models/autonomous-car-{}-{}-{}-epochs-50K-data.model'.format(LR, 'alexnet',EPOCHS)
     
 model = alexnet(WIDTH, HEIGHT, LR)
 model.load(MODEL_NAME)
@@ -47,19 +47,16 @@ def test_nn():
 
             turn_thresh = .75
             forward_thresh = .70
-            reverse_thresh = .70
             
             if prediction[0] > forward_thresh:
                 p_key(W)
-            elif prediction[1] > reverse_thresh:
-                p_key(S)
-            elif prediction[2] > turn_thresh:
+            elif prediction[1] > turn_thresh:
                 p_key(A)
-            elif prediction[3] > turn_thresh:
+            elif prediction[2] > turn_thresh:
                 p_key(D)
-            elif prediction[4] > turn_thresh:
+            elif prediction[3] > turn_thresh:
                 p_key(Q)
-            elif prediction[5] > turn_thresh:
+            elif prediction[4] > turn_thresh:
                 p_key(E)
             else:
                 p_key(W)
