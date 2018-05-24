ac1fb5c3ce44b5ca85a732d0eeacba373d0ae2e4
PosticaDenis <postica.denis.95@gmail.com>
2018-05-24 21:41:17 +0300

    Update train_nn.py

--- train_nn.py
+++ train_nn.py
@@ -8,8 +8,8 @@ Created on Wed May 23 08:44:53 2018
 import numpy as np
 from alexnet import alexnet
 
-WIDTH = 292
-HEIGHT = 255
+WIDTH = 90
+HEIGHT = 80
 
 LR = 1e-3  # learning rate
 
@@ -21,13 +21,13 @@ data_fname  = 'collected_data/training_data-{}.npy'
 
 model = alexnet(WIDTH, HEIGHT, LR)
 
-data_files = 22
+data_files = 10
 for i in range(EPOCHS):
     for i in range(1, data_files + 1):
         train_data = np.load(data_fname.format(i))
 
-        train = train_data[:-100]
-        test = train_data[-100:]
+        train = train_data[:-250]
+        test = train_data[-250:]
 
         X = np.array([i[0] for i in train]).reshape(-1, WIDTH, HEIGHT, 1)
         Y = [i[1] for i in train]
@@ -35,10 +35,11 @@ for i in range(EPOCHS):
         test_x = np.array([i[0] for i in test]).reshape(-1, WIDTH, HEIGHT, 1)
         test_y = [i[1] for i in test]
 
-        model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}), 
-            snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+        model.fit({'input': X}, {'targets': Y}, n_epoch = 1, validation_set = ({'input': test_x}, {'targets': test_y}), 
+            snapshot_step = 500, show_metric = True, run_id = MODEL_NAME)
 
         model.save(MODEL_NAME)
+        print('Saved model.')
 
 
 # tensorboard --logdir=foo:C:/path/to/log
