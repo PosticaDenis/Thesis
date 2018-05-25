99868f36f093d35c16afd7a286fe735bc0d40fd3
PosticaDenis <postica.denis.95@gmail.com>
2018-05-25 11:42:16 +0300

    Update test_nn.py

--- test_nn.py
+++ test_nn.py
@@ -4,22 +4,17 @@ Created on Thu May 24 02:55:32 2018
 
 @author: denis
 """
-
 import cv2
 import time
 from tools.keyboard_controller import PressKey, ReleaseKey, W, A, D, Q, E
-# from alexnet import alexnet
 from models import inception_v3 as googlenet
 from tools.keys import check_key
 from tools.screen import get_screen
 
 WIDTH = 90
 HEIGHT = 80
-
 LR = 1e-3
-
 EPOCHS = 10
-
 MODEL_NAME = ''
 model = googlenet(WIDTH, HEIGHT, 1, LR, output = 6, model_name=MODEL_NAME)
 #model = alexnet(WIDTH, HEIGHT, LR)
@@ -33,21 +28,16 @@ def test_nn():
     for i in list(range(3))[::-1]:
         print(i+1)
         time.sleep(1)
-
     paused = False
     while(True):
-        
         if not paused:
             screen = get_screen(region = (20, 150, 975, 850))
             screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
             screen = cv2.resize(screen, (WIDTH, HEIGHT))
-
             prediction = model.predict([screen.reshape(WIDTH, HEIGHT, 1)])[0]
             print(prediction)
-
             turn_thresh = .75
             forward_thresh = .70
-            
             if prediction[0] > forward_thresh:
                 p_key(W)
             elif prediction[1] > turn_thresh:
@@ -60,9 +50,7 @@ def test_nn():
                 p_key(E)
             else:
                 p_key(W)
-
         keys = check_key()
-
         if 'P' in keys:
             if paused:
                 paused = False
