cad2c84c4d051eab2bee07c497767c8506187251
PosticaDenis <postica.denis.95@gmail.com>
2018-05-25 11:44:04 +0300

    Update server.py

--- car/server.py
+++ car/server.py
@@ -4,20 +4,16 @@ import car_controller as car
 TF = 0.035
 UDP_IP = "192.168.1.5"
 UDP_PORT = 5055
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
-
 options = ['w', 'a', 'd', 'q', 'e']
 
 try:
     while True:
         option, address = sock.recvfrom(256)
         option = option.decode()
-        
         if option in options:
             print("Received command: ", option)
-            
             if option == 'w':
                 car.forward(TF, 30, 30, 30, 30)
             elif option == 'a':
@@ -31,8 +27,6 @@ try:
             else:
                 car.stop()
         else:
-            car.stop()
-        
+            car.stop() 
 except KeyboardInterrupt:
     sock.close()
-    
